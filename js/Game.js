f6ef9d6a4b4ae374d5053e6d26531d0adb685eaf
Thiago Saraiva <saraii_@live.com>
2021-12-21 11:27:55 -0300

    upload

---
+++ js/Game.js
@@ -0,0 +1,84 @@
+class Game {
+  constructor() {}
+  //BP
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+  //BP
+  update(state) {
+    database.ref("/").update({
+      gameState: state
+    });
+  }
+
+  // AP
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+
+    car1 = createSprite(width / 2 - 50, height - 100);
+    car1.addImage("carro1", car1_img);
+    car1.scale = 0.07;
+
+    car2 = createSprite(width / 2 + 100, height - 100);
+    car2.addImage("carro2", car2_img);
+    car2.scale = 0.07;
+
+    cars = [car1, car2];
+  }
+
+  //BP
+  handleElements() {
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+  }
+
+  //SA
+  play() {
+    this.handleElements();
+
+    Player.getPlayersInfo(); //added
+
+    if (allPlayers !== undefined) {
+      image(track, 0, -height * 5, width, height * 6);
+
+      //índice da matriz
+      var index = 0;
+      for (var plr in allPlayers) {
+        //use os dados do banco de dados para exibir os carros nas direções x e y
+        var x = allPlayers[plr].positionX;
+        var y = height - allPlayers[plr].positionY;
+
+        cars[index].position.x = x;
+        cars[index].position.y = y;
+
+        //adicione 1 ao índice de cada loop
+        index = index + 1;
+
+        if (index === player.index) {
+          stroke(10);
+          fill("red");
+          ellipse(x, y, 60, 60);
+
+          
+
+        }
+      }
+
+      // manipule os eventos do teclado
+      if (keyIsDown(UP_ARROW)) {
+        player.positionY += 10;
+        player.update();
+      }
+ 
+      drawSprites();
+    }
+  }
+}
